ba72ac58c4008f373dbc7a81c59d7d6eed423b38
Davyjones <ekenedavidson15@gmail.com>
2022-01-11 20:50:27 +0100

    new

--- src/main/java/com/sabi/supplier/api/SabiSupplierApiApplication.java
+++ src/main/java/com/sabi/supplier/api/SabiSupplierApiApplication.java
@@ -5,10 +5,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @ComponentScan(basePackages = "com.sabi.framework")
 @EntityScan(basePackages = {"com.sabisupplierscore.models"})
 @EnableJpaRepositories({"com.sabi.supplier.service.repositories","com.sabi.framework.repositories"})
+@EnableTransactionManagement
 @SpringBootApplication
 public class SabiSupplierApiApplication {
 
