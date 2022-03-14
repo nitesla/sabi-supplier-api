76c215ca3c413b2b2b3d7313216ec06fc3b5b5b8
muyiwa_dev <muyiwa.a@spinel.consulting>
2022-03-14 03:19:04 +0100

    analytics dashboard update with date range.

--- src/main/java/com/sabi/supplier/api/controllers/SupplierDashboardController.java
+++ src/main/java/com/sabi/supplier/api/controllers/SupplierDashboardController.java
@@ -10,11 +10,13 @@ import com.sabi.suppliers.core.models.response.DashboardWarehouseResponse;
 import com.sabi.suppliers.core.models.response.StockResponseDto;
 import com.sabi.suppliers.core.models.response.SupplierDashbaordResponseDto;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @SuppressWarnings("All")
@@ -38,12 +40,16 @@ public class SupplierDashboardController {
         return new ResponseEntity<>(resp, httpCode);
     }
 
-    @PostMapping("summary/product/{supplierId}")
+    @PostMapping("summary/product")
     // @PreAuthorize("hasAnyRole('ROLE_SUPER_ADMIN','ROLE_CREATE_USER')")
-    public ResponseEntity<Response> getTopProduct(@PathVariable Long supplierId){
+    public ResponseEntity<Response> getTopProduct(@RequestParam(value = "supplierId") Long supplierId,
+                                                  @RequestParam(value = "startDate") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime startDate,
+                                                  @RequestParam(value = "endDate") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime endDate
+
+    ){
         HttpStatus httpCode ;
         Response resp = new Response();
-        List<DashboardProductResponse> response = dashboardService.fetchTopProduct(supplierId);
+        List<DashboardProductResponse> response = dashboardService.fetchTopProduct(supplierId,startDate,endDate);
         resp.setCode(CustomResponseCode.SUCCESS);
         resp.setDescription("Successful");
         resp.setData(response);
@@ -51,12 +57,28 @@ public class SupplierDashboardController {
         return new ResponseEntity<>(resp, httpCode);
     }
 
-    @PostMapping("summary/warehouse/{productId}")
+//    @GetMapping("summary/warehouse/{productId}")
+//    // @PreAuthorize("hasAnyRole('ROLE_SUPER_ADMIN','ROLE_CREATE_USER')")
+//    public ResponseEntity<Response> getWarehouse(@PathVariable Long productId){
+//        HttpStatus httpCode ;
+//        Response resp = new Response();
+//        List<DashboardWarehouseResponse> response = dashboardService.fetchWarehouseProduct(productId);
+//        resp.setCode(CustomResponseCode.SUCCESS);
+//        resp.setDescription("Successful");
+//        resp.setData(response);
+//        httpCode = HttpStatus.CREATED;
+//        return new ResponseEntity<>(resp, httpCode);
+//    }
+
+    @GetMapping("summary/warehouseDate")
     // @PreAuthorize("hasAnyRole('ROLE_SUPER_ADMIN','ROLE_CREATE_USER')")
-    public ResponseEntity<Response> getWarehouse(@PathVariable Long productId){
+    public ResponseEntity<Response> getWarehouseDate(@RequestParam(value = "productId") Long productId,
+                                                     @RequestParam(value = "startDate") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime startDate,
+                                                     @RequestParam(value = "endDate") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime endDate
+    ){
         HttpStatus httpCode ;
         Response resp = new Response();
-        List<DashboardWarehouseResponse> response = dashboardService.fetchWarehouseProduct(productId);
+        List<DashboardWarehouseResponse> response = dashboardService.fetchWarehouseProductDate(productId,startDate,endDate);
         resp.setCode(CustomResponseCode.SUCCESS);
         resp.setDescription("Successful");
         resp.setData(response);
