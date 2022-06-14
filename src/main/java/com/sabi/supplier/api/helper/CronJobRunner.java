a0020083f90abf918d7acfd32579d6e99d999155
muyiwa_dev <muyiwa.a@spinel.consulting>
2022-06-14 23:04:33 +0100

    change..

--- src/main/java/com/sabi/supplier/api/helper/CronJobRunner.java
+++ src/main/java/com/sabi/supplier/api/helper/CronJobRunner.java
@@ -1,27 +1,27 @@
-//package com.sabi.supplier.api.helper;
-//
-//
-//import com.sabi.supplier.service.services.SupplyRequestService;
-//import lombok.extern.slf4j.Slf4j;
-//import org.springframework.scheduling.annotation.Scheduled;
-//import org.springframework.stereotype.Service;
-//
-//import java.util.Date;
-//
-//@Slf4j
-//@Service
-//public class CronJobRunner {
-//
-//    private SupplyRequestService supplyRequestService;
-//
-//    public CronJobRunner(SupplyRequestService supplyRequestService) {
-//        this.supplyRequestService = supplyRequestService;
-//    }
-//
-//    // @Scheduled(cron="0 0 0 * * ?")
-//    @Scheduled(cron="${cronExpressionSupplier}")
-//    public void expireAcceptedAndExpiredTrips() {
-//        log.info("::::::::::::: Cron Job Started at :::::::::::: :   %s", new Date());
-//        supplyRequestService.pushToIncomingRequest();
-//    }
-//}
+package com.sabi.supplier.api.helper;
+
+
+import com.sabi.supplier.service.services.SupplyRequestService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+
+@Slf4j
+@Service
+public class CronJobRunner {
+
+    private SupplyRequestService supplyRequestService;
+
+    public CronJobRunner(SupplyRequestService supplyRequestService) {
+        this.supplyRequestService = supplyRequestService;
+    }
+
+    // @Scheduled(cron="0 0 0 * * ?")
+    @Scheduled(cron="${cronExpressionSupplier}")
+    public void expireAcceptedAndExpiredTrips() {
+        log.info("::::::::::::: Cron Job Started at :::::::::::: :   %s", new Date());
+        supplyRequestService.pushToIncomingRequest();
+    }
+}
